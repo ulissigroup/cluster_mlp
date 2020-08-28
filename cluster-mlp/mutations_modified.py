c3d1be1993cb9a9db4ca6eac0769b123ac3ca30f
Saurabh Sivakumar <saurabhsiva@cmu.edu>
2020-08-28 14:57:38 -0400

    Added FixOverlap function

--- cluster-mlp/mutations_modified.py
+++ cluster-mlp/mutations_modified.py
@@ -9,47 +9,49 @@ from ase.constraints import FixAtoms
 from ase.data import atomic_numbers,vdw_radii
 
 def get_data(cluster):
-	eleNames = list(set(cluster.get_chemical_symbols()))
-	eleNums = []
-	for i,name in enumerate(eleNames):
-		ele_count = 0
-		for a in cluster.get_chemical_symbols():
-			if a == eleNames[i]:
-				ele_count += 1
-		eleNums.append(ele_count)
-	natoms = len(cluster)
-	stride = natoms + 2
-	eleRadii = []
-	atomic_numbers_list = []
-	for i in range(len(eleNames)):
-		atomic_num = atomic_numbers[eleNames[i]]
-		atomic_numbers_list.append(atomic_num)
-		eleR = vdw_radii[atomic_num]
-		eleRadii.append(eleR)
+		eleNames = list(set(cluster.get_chemical_symbols()))
+		eleNums = []
+		for i,name in enumerate(eleNames):
+			ele_count = 0
+			for a in cluster.get_chemical_symbols():
+				if a == eleNames[i]:
+					ele_count += 1
+			eleNums.append(ele_count)
+		natoms = len(cluster)
+		stride = natoms + 2
+		eleRadii = []
+		atomic_numbers_list = []
+		for i in range(len(eleNames)):
+			atomic_num = atomic_numbers[eleNames[i]]
+			atomic_numbers_list.append(atomic_num)
+			eleR = vdw_radii[atomic_num]
+			eleRadii.append(eleR)
 
-	eleRadii = eleRadii
+		return eleNames,eleNums,natoms,stride,eleRadii
 
-	return eleNames,eleNums,natoms,stride,eleRadii
+def fixOverlap(clus):
+		natoms = len(clus)
+		com = clus.get_center_of_mass()
+		clus.center(about = com)
+		for i in range(natoms):
+		        for j in range(i):
+		            r1 = np.array(clus[j].position)
+		            r2 = np.array(clus[i].position)
+		            rij = r2 - r1
+		            distance = np.sqrt(np.dot(rij, rij))
+		            dmin = vdw_radii[clus[i].number] + vdw_radii[clus[j].number]
+		            if distance < 0.9 * dmin:
+		                a = np.dot(r2, r2)
+		                b = np.dot(r1, r2)
+		                c = np.dot(r1, r1) - dmin**2
+		                alpha = 1.000001 * (b + np.sqrt(b * b - a * c)) / a
+		                clus[i].x *= alpha
+		                clus[i].y *= alpha
+		                clus[i].z *= alpha
+		return clus
 
-class mutations:
-	def __init__(self,eleNames,eleNums,natoms,stride,eleRadii,mutType):
-		self.eleNames = eleNames
-		self.eleNums = eleNums
-		self.natoms = sum(eleNums)
-		self.stride = self.natoms + 2
-		self.eleRadii = eleRadii
-		self.nPool = nPool
-		self.mutType = mutType
-		self.surfGA = surfGA
 
-		ave = 0.0
-		for i in range(len(eleNums)):
-			for j in range(eleNums[i]):
-				ave += eleRadii[i] / self.natoms
-		self.radave = ave
-
-
-	def add_atoms(self,clusm,atcenter):
+def add_atoms(clusm,atcenter):
 		'''
 		Add atom(s) to a smaller cluster given in poolm.dat
 		'''
@@ -66,14 +68,16 @@ class mutations:
 
 		r = max(rlist) + 0.5
 
-		for i in range(len(self.eleNames)):
-				ele = self.eleNames[i]
+		eleNames,eleNums,natoms,stride,eleRadii = get_data(clusm)
+
+		for i in range(len(eleNames)):
+				ele = eleNames[i]
 				n = 0
 				for elem in eleList:
 					if ele == elem:
 						n += 1
 
-				while n < self.eleNums[i]:
+				while n < eleNums[i]:
 					if atcenter:
 						added_atom = Atoms(ele,positions = [[0.0,0.0,0.0]])
 						atcenter = False
@@ -87,137 +91,112 @@ class mutations:
 
 					clusm += added_atom
 					n += 1
-		clus = pre.fixOverlap(clusm,self.eleNames,self.eleRadii)
+
+		clus = fixOverlap(clusm)
 		return clus
-	def mutate(self): #BASE DONE, NOT YET FULLY FIXED
-			pair,fitpair,parents = self.choosePair()
-			clus = pair[0]
-			parent = parents[0]
-			if self.mutType == 'All':
-				mType = ran.choice(['Rattle','Twist','Tunnel','Rotate'])
-			else:
-				mType = self.mutType
 
-			if len(self.eleNames) == 1:
-				mono = True
-			else:
-				mono = False
+def homotop(clus): #DONE
+		'''
+		Choose pair of different elements to swap
+		'''
+		eleNames,eleNums,natoms,stride,eleRadii = get_data(clus)
+		eles = ran.sample(eleNames,2)
+		ele1_index = []
+		ele2_index = []
+		for i in clus:
+			if i.symbol == eles[0]:
+				ele1_index.append(i.index)
+			if i.symbol == eles[1]:
+				ele2_index.append(i.index)
 
-			if not mono and mType == 'Homotop':
-				mutant = self.homotop(clus)
-
-			elif mType == 'Rattle':
-				mutant = self.rattle_mut(clus)
-
-			elif mType == 'Twist':
-				mutant = self.twist(clus)
-
-			elif mType == 'Tunnel':
-				mutant = self.tunnel(clus)
-
-			elif mType == 'Rotate':
-				mutant = self.rotate_mut(clus,self.surfGA)
-
-			else:
-				mutant = clus
-
-			return mutant, parent, mType
-
-	def homotop(self,clus): #DONE
-			'''
-			Choose pair of different elements to swap
-			'''
-			eles = ran.sample(self.eleNames,2)
-			ele1_index = []
-			ele2_index = []
-			for i in clus:
-				if i.symbol == eles[0]:
-					ele1_index.append(i.index)
-				if i.symbol == eles[1]:
-					ele2_index.append(i.index)
-
-			ele1_position = ran.choice(ele1_index)
-			ele2_position = ran.choice(ele2_index)
-			clus.positions[[ele1_position,ele2_position]] = clus.positions[[ele2_position,ele1_position]]
-			return clus
+		ele1_position = ran.choice(ele1_index)
+		ele2_position = ran.choice(ele2_index)
+		clus.positions[[ele1_position,ele2_position]] = clus.positions[[ele2_position,ele1_position]]
+		return clus
 
 
-	def rattle_mut(self,clus): #DONE
-			'''
-			Choose one atom and move 1/4 of the cluster atoms
-			nearest to that atom a distance between 0 and
-			+/- its radius
-			'''
-			#CHANGED TO FIX A THIRD OF ATOMS AND RATTLE REST
-			indices = ran.sample(range(len(clus)), int(len(clus)/3))
-			const = FixAtoms(indices = indices)
-			clus.set_constraint(const)
-			clus.rattle()
-			del clus.constraints
+def rattle_mut(clus): #DONE
+		'''
+		Choose one atom and move 1/4 of the cluster atoms
+		nearest to that atom a distance between 0 and
+		+/- its radius
+		'''
+		#CHANGED TO FIX A THIRD OF ATOMS AND RATTLE REST
+		indices = ran.sample(range(len(clus)), int(len(clus)/3))
+		const = FixAtoms(indices = indices)
+		clus.set_constraint(const)
+		clus.rattle()
+		del clus.constraints
 
-			pre.fixOverlap(clus,self.eleNames,self.eleRadii)
+		clus = fixOverlap(clus)
 
-			return clus
-
-
-	def twist(self,clus):
-			'''
-			Twist the cluster
-			'''
-			#ROTATE ALONG X FIXING Y AND Z
-			clus.rotate('y','z',center = 'COP')
-
-			pre.fixOverlap(clus,self.eleNames,self.eleRadii)
-			return clus
+		return clus
 
 
-	def tunnel(self,clus):
-			'''
-			Tunnel one of the atoms farthest from the center to
-			the other side of the cluster
-			'''
-			#FIND THE DISTANCES OF ALL ATOMS TO EACH OTHER,FIND 2 ATOMS WITH THE GREATEST DISTANCE
-			#AND TUNNEL ONE OF THEM TO THE OPPOSITE SIDE
-			distances = clus.get_all_distances()
-			max_indices = np.unravel_index(distances.argmax(),distances.shape)
-			atom_index = ran.choice(max_indices)
-			x = clus[atom_index].x
-			y = clus[atom_index].y
-			z = clus[atom_index].z
-			clus.positions[atom_index] = (-x,-y,-z)
+def twist(clus):
+		'''
+		Twist the cluster
+		'''
+		#ROTATE ALONG X FIXING Y AND Z
+		clus.rotate('y','z',center = 'COP')
 
-			#OR
-
-			for i in range(len(clus)):
-				flag = np.isclose(clus.get_center_of_mass(),clus[i].position,atol = 1e-1,rtol = 1e-1) #TRICKY NOT SURE IF IT WILL WORK
-				check = (flag[0] and flag[1]) or flag[2]
-				if check == True:
-					center = clus[i].position
-					break
-			distances = clus.get_distances(center,indices = np.array([np.arange(0,len(clus))]))
-			max_index = np.argmax(distances)
-			x = clus[max_index].x
-			y = clus[max_index].y
-			z = clus[max_index].z
-			clus.positions[max_index] = (-x,-y,-z)
-
-			pre.fixOverlap(clus,self.eleNames,self.eleRadii)
-			return clus
+		clus = fixOverlap(clus)
+		return clus
 
 
-	def rotate_mut(self,clus):
-			'''
-			Two modes for rotate mutation
+def tunnel(clus):
+		'''
+		Tunnel one of the atoms farthest from the center to
+		the other side of the cluster
+		'''
+		center = clus.get_center_of_mass()
+		distances = []
+		for atom in clus:
+			distance = np.sqrt(np.dot(center - atom.position))
+			distances.append(distance)
 
-			1 - If complete, rotate entire cluster
-			2 - If incomplete, rotate 25% of cluster atoms
-			'''
-			#MODIFIED TO ALWAYS ROTATE FULL CLUSTER
-			angle = ran.randint(1,180)
-			axis = ran.choice(['x','y','z'])
-			clus.rotate(angle,axis,center = 'COP')
-			pre.fixOverlap(clus,self.eleNames,self.eleRadii)
+		distances = np.array(distances)
+		max_index = np.argmax(distances)
+		x = clus[max_index].x
+		y = clus[max_index].y
+		z = clus[max_index].z
+		clus.positions[max_index] = (-x,-y,-z)
 
-			return clus
+		clus = fixOverlap(clus)
+		return clus
 
 
+def rotate_mut(clus):
+		'''
+		Two modes for rotate mutation
+
+		1 - If complete, rotate entire cluster
+		2 - If incomplete, rotate 25% of cluster atoms
+		'''
+		#MODIFIED TO ALWAYS ROTATE FULL CLUSTER
+		angle = ran.randint(1,180)
+		axis = ran.choice(['x','y','z'])
+		clus.rotate(angle,axis,center = 'COP')
+		clus = fixOverlap(clus)
+
+		return clus
+
+def partialInversion(clus):
+		'''
+		Choose a fragment with 30% of the cluster atoms
+		nearest to a randomly chosen atom and invert the
+		structure with respect to its geometrical center
+		'''
+		selected_atom = ran.choice(clus)
+		distances = clus.get_distances(selected_atom.index,np.arange(0,len(clus)))
+		ncount = int(0.3*len(clus))
+		idx = []
+		idx.append(np.argsort(distances)[-ncount:])
+		for i in idx:
+			x = clus[idx].x
+			y = clus[idx].y
+			z = clus[idx].z
+			clus.positions[idx] = (-x,-y,-z)
+
+		clus = fixOverlap(clus)
+		return clus
