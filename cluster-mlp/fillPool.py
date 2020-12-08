e24efe514f196bfc3794f9352b504f6b8e334e77
Rajesh <rajesh444@gmail.com>
2020-12-08 16:57:39 -0500

    removed commented out code lines

--- cluster-mlp/fillPool.py
+++ cluster-mlp/fillPool.py
@@ -6,11 +6,9 @@ def fillPool(eleNames,eleNums,eleRadii,calc):
 	'''
 	Fill Initial pool with random geometries
 	'''
-	#ini_pool = []
 	ele_initial = [eleNames[0], eleNames[-1]]
 	d = (eleRadii[0] + eleRadii[-1])/2
 	clusm = Atoms(ele_initial, [(-d, 0.0, 0.0), (d, 0.0, 0.0)])
 	clus = addAtoms(clusm,eleNames,eleNums,eleRadii )
-	#clus = fixOverlap(clus)
 	return  clus
 
